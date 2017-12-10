3833b14569c01903054b1dbd944ea0b4ce733173
jpchaillot <jpchaillot@gmail.com>
2017-12-10 09:56:37 +0100

    ecran_led

--- autorun/autorun.sh
+++ autorun/autorun.sh
@@ -2,15 +2,15 @@
 
 until [ ]
 do
-	for image in `ls /home/pi/rpi-rgb-led-matrix/autorun/*.ppm 2>/dev/null`
+	for image in `ls /home/pi/ecran_led/autorun/*.ppm 2>/dev/null`
 	do
 		largeur=`identify -format %w $image`
 		let "temps = largeur/32 +2"
-		sudo /home/pi/rpi-rgb-led-matrix/examples-api-use/demo -t $temps -D1 $image --led-chain=3
+		sudo /home/pi/ecran_led/examples-api-use/demo -t $temps -D1 $image --led-chain=3
 	done
 
-	for animation in `ls /home/pi/rpi-rgb-led-matrix/autorun/*.gif 2>/dev/null`
+	for animation in `ls /home/pi/ecran_led/autorun/*.gif 2>/dev/null`
 	do
-		sudo /home/pi/rpi-rgb-led-matrix/utils/led-image-viewer -l 3 $animation
+		sudo /home/pi/ecran_led/utils/led-image-viewer -l 3 $animation
 	done
 done
